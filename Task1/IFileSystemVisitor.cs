469ac38c7020a7de057c7623c84909eab050380b
Aliaksandr Fiodarau <Aliaksandr_Fiodarau2@epam.com>
2017-10-16 22:19:18 +0300

    Add events into IFileSystemVisitor

--- Task1/IFileSystemVisitor.cs
+++ Task1/IFileSystemVisitor.cs
@@ -1,8 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Task1
 {
     public interface IFileSystemVisitor : IEnumerable<string>
     {
+        event EventHandler Start;
+
+        event EventHandler Finish;
+
+        event EventHandler<VisitArgs> FileFound;
+
+        event EventHandler<VisitArgs> FilteredFileFound;
+
+        event EventHandler<VisitArgs> DirectoryFound;
+
+        event EventHandler<VisitArgs> FilteredDirectoryFound;
     }
 }
